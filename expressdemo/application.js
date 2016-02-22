49cb78cc628b3beb5eb692798452d5b0adb889c2
vishipayyallore <vishipayyallore@yahoo.com>
2016-02-22 14:31:42 +0530

    AAA

--- expressdemo/application.js
+++ expressdemo/application.js
@@ -1,14 +1,16 @@
+'use strict';
+
 var express = require('express');
 
 var app = express.createServer();
 
 var products = `
 { "products": [{ "name": "Pen", "price": 10.23 }, 
-{ "name": "Books", "price": 12.85 }] }
+{ "name": "Books of Ranga", "price": 12.85 }] }
 `;
 
 app.get('/expressdemo/api', function(req, res){
-    res.send('Welcome to Cocktail NodeJS API!');
+    res.send('Welcome to Ranga NodeJS API!');
 });
 
 app.get('/expressdemo/api/healthcheck', function (req, res) {
@@ -20,3 +22,5 @@ app.get('/expressdemo/api/products', function (req, res) {
 });
 
 app.listen(process.env.PORT);
+
+module.exports = app;
