ff57f8050937c75f7df869b491dde31b82128ca1
vishipayyallore <vishipayyallore@yahoo.com>
2016-02-22 13:32:40 +0530

    Initial Commit

---
+++ expressdemo/hello.js
@@ -0,0 +1,22 @@
+var express = require('express');
+
+var app = express.createServer();
+
+var products = `
+{ "products": [{ "name": "Pen", "price": 10.23 }, 
+{ "name": "Books", "price": 12.85 }] }
+`;
+
+app.get('/expressdemo/api', function(req, res){
+    res.send('Welcome to Cocktail NodeJS API!');
+});
+
+app.get('/expressdemo/api/healthcheck', function (req, res) {
+    res.send('Express Web API\'s Health Check is Good.');
+});
+
+app.get('/expressdemo/api/products', function (req, res) {
+    res.json(products);
+});
+
+app.listen(process.env.PORT);
